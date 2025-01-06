d6d2082e372f7a8bdf57917ac6279a8c20c689a1
zyerin <zyerin5974@gmail.com>
2025-01-06 20:37:59 +0900

    feat: SecurityConfig - 스프링 시큐리티 설정

--- src/main/java/com/goldang/goldangtime/config/SecurityConfig.java
+++ src/main/java/com/goldang/goldangtime/config/SecurityConfig.java
@@ -1,2 +1,43 @@
-package com.goldang.goldangtime.config;public class SecurityConfig {
+package com.goldang.goldangtime.config;
+
+import com.goldang.goldangtime.config.jwt.JwtAuthenticationFilter;
+import com.goldang.goldangtime.config.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.httpBasic((hp)->hp.disable())
+                // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
+                .csrf((cs)->cs.disable())
+                // JWT를 사용하기 때문에 세션을 사용하지 않음
+                .sessionManagement((sm)->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((ahr)->ahr.requestMatchers("/user/**").permitAll())
+                // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt Encoder 사용
+        return new BCryptPasswordEncoder();
+    }
+
 }
