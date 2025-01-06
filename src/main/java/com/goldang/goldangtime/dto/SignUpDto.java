a5035bc54bf60f7c8aced1c7dccd841c76adb8ec
zyerin <zyerin5974@gmail.com>
2025-01-06 20:41:00 +0900

    feat: SignUpDto 생성

--- src/main/java/com/goldang/goldangtime/dto/SignUpDto.java
+++ src/main/java/com/goldang/goldangtime/dto/SignUpDto.java
@@ -1,2 +1,16 @@
-package com.goldang.goldangtime.dto;public class SignUpDto {
+package com.goldang.goldangtime.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class SignUpDto {
+    private String email;
+    private String password;
+    private String nickname;
 }
