63e0e17e45bff97069c03e85d35cdce0aeeda4d7
zyerin <zyerin5974@gmail.com>
2025-01-08 14:40:08 +0900

    feat: PetController 생성 - 반려동물 정보 저장

--- src/main/java/com/goldang/goldangtime/controller/PetController.java
+++ src/main/java/com/goldang/goldangtime/controller/PetController.java
@@ -1,2 +1,23 @@
-package com.goldang.goldangtime.controller;public class PetController {
+package com.goldang.goldangtime.controller;
+
+import com.goldang.goldangtime.dto.PetRequestDto;
+import com.goldang.goldangtime.entity.Pets;
+import com.goldang.goldangtime.service.PetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pets")
+@RequiredArgsConstructor
+public class PetController {
+
+    private final PetService petService;
+
+    @PostMapping("/save")
+    public ResponseEntity<Pets> savePet(@RequestBody PetRequestDto petRequestDto) {
+        Pets pet = petService.savePet(petRequestDto);
+        return ResponseEntity.ok(pet);
+    }
 }
+
