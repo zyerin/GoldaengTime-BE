5916fd4da513e26b6d8fb6dc9daa831cd49671a7
zyerin <zyerin5974@gmail.com>
2025-01-09 23:02:52 +0900

    feat: FCM을 사용하기 위해 Firebase SDK를 초기화하는 FirebaseConfig 설정파일 추가

--- src/main/java/com/goldang/goldangtime/config/FirebaseConfig.java
+++ src/main/java/com/goldang/goldangtime/config/FirebaseConfig.java
@@ -1,2 +1,31 @@
-package com.goldang.goldangtime.config;public class FirebaseConfig {
+package com.goldang.goldangtime.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            var serviceAccount = new ClassPathResource("firebase/goldaengtime-firebase-adminsdk.json").getInputStream();
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            if(FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
