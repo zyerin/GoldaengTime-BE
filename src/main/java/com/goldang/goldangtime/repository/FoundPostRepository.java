20b4d7febb87fa84da184c00d9506e8489698c60
zyerin <zyerin5974@gmail.com>
2025-01-09 22:58:15 +0900

    feat: 게시글 관련 리포지토리 파일 생성

--- src/main/java/com/goldang/goldangtime/repository/FoundPostRepository.java
+++ src/main/java/com/goldang/goldangtime/repository/FoundPostRepository.java
@@ -1,2 +1,11 @@
-package com.goldang.goldangtime.repository;public class FoundPostRepository {
+package com.goldang.goldangtime.repository;
+
+import com.goldang.goldangtime.entity.FoundPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoundPostRepository extends JpaRepository<FoundPost, Long> {
 }
+
+
