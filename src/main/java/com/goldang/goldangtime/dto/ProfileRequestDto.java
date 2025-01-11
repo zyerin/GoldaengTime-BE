a08154a1fd8c1bbb8b361499fc2960082a4aae70
zyerin <zyerin5974@gmail.com>
2025-01-12 00:45:34 +0900

    fix: /user/** 경로 인증 없이 허용하도록 추가

---
+++ src/main/java/com/goldang/goldangtime/dto/ProfileRequestDto.java
@@ -0,0 +1,11 @@
+package com.goldang.goldangtime.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MyPageDto {
+    private String nickname;
+    private String userPhoto;
+}
