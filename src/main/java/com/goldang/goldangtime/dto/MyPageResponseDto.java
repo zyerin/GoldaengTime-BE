574290adfa2ea423927fcd1a129a7905149a8e3a
zyerin <zyerin5974@gmail.com>
2025-01-12 01:03:33 +0900

    feat: 마이페이지에서 사용자가 작성한 글과 스크랩한 게시글을 반환할 Dto 생성

--- src/main/java/com/goldang/goldangtime/dto/MyPageResponseDto.java
+++ src/main/java/com/goldang/goldangtime/dto/MyPageResponseDto.java
@@ -1,2 +1,17 @@
-package com.goldang.goldangtime.dto;public class MyPageResponseDto {
+package com.goldang.goldangtime.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class MyPageResponseDto {
+    private Long id;           // 게시글 ID
+    private String type;       // 게시글 유형 ("LostPost" 또는 "FoundPost")
+    private String title;
+    private String description;
+    private String photo;
+    private LocalDateTime createdAt;
 }
